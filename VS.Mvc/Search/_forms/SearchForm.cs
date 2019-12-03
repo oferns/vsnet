07400a7dfebb3880d5815154d8d1aa0d8aba5ca8
Oliver Ferns <oferns@gmail.com>
2019-12-03 10:31:48 +0000

    Search form

---
+++ VS.Mvc/Search/_forms/SearchForm.cs
@@ -0,0 +1,17 @@
+﻿namespace VS.Mvc.Search._Forms {
+
+    using Microsoft.AspNetCore.Mvc;
+    using VS.Mvc._Extensions;
+        
+    [ViewComponent(Name="SearchForm")]
+    public class SearchForm : LocalizedViewComponent {
+
+        public IViewComponentResult Invoke() {
+
+            var qs = ViewContext.HttpContext.Request.Query["q"];
+            return View(qs);
+        }
+
+
+    }
+}
