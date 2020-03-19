054767027a18f0fe9b1e1b50ca279d9b27367180
Ollie Ferns <ollie@nlsltd.com>
2020-03-19 17:13:23 +0000

    More assets and fonts

--- VS.Mvc/_Startup/MvcRoutes.cs
+++ VS.Mvc/_Startup/MvcRoutes.cs
@@ -20,115 +20,115 @@
 
         public static IEndpointRouteBuilder AddMvcEndpoints(this IEndpointRouteBuilder builder) {
 
-            // // generic routes
-            // builder.MapDynamicControllerRoute<LocalizedRouteValueTransformer>(
-            //    "{area:exists:slugify}/{eid:int}/{subarea:exists:slugify}/{controller:slugify}/{childid:int}/{action:slugify}"
-            // );
+            // generic routes
+            builder.MapDynamicControllerRoute<LocalizedRouteValueTransformer>(
+               "{area:exists:slugify}/{eid:int}/{subarea:exists:slugify}/{controller:slugify}/{childid:int}/{action:slugify}"
+            );
 
-            // builder.MapDynamicControllerRoute<LocalizedRouteValueTransformer>(
-            //     "{area:exists:slugify}/{subarea:exists:slugify}/{controller:slugify}/{action:slugify}"
-            // );
+            builder.MapDynamicControllerRoute<LocalizedRouteValueTransformer>(
+                "{area:exists:slugify}/{subarea:exists:slugify}/{controller:slugify}/{action:slugify}"
+            );
 
-            // builder.MapDynamicControllerRoute<LocalizedRouteValueTransformer>(
-            //    "{area:exists:slugify}/{eid:int}/{subarea:exists:slugify}/{controller:slugify}/{action:slugify}"
-            // );
+            builder.MapDynamicControllerRoute<LocalizedRouteValueTransformer>(
+               "{area:exists:slugify}/{eid:int}/{subarea:exists:slugify}/{controller:slugify}/{action:slugify}"
+            );
 
-            // builder.MapDynamicControllerRoute<LocalizedRouteValueTransformer>(
-            //    "{area:exists:slugify}/{eid:int}/{controller:slugify}/{childid:int}/{action:slugify}"
-            // );
+            builder.MapDynamicControllerRoute<LocalizedRouteValueTransformer>(
+               "{area:exists:slugify}/{eid:int}/{controller:slugify}/{childid:int}/{action:slugify}"
+            );
 
-            // builder.MapDynamicControllerRoute<LocalizedRouteValueTransformer>(
-            //    "{area:exists:slugify}/{eid:int}/{controller:slugify}/{action:slugify}"
-            // );
+            builder.MapDynamicControllerRoute<LocalizedRouteValueTransformer>(
+               "{area:exists:slugify}/{eid:int}/{controller:slugify}/{action:slugify}"
+            );
 
-            // builder.MapDynamicControllerRoute<LocalizedRouteValueTransformer>(               
-            //    "{area:exists:slugify}/{controller:slugify}/{eid:int}/{action:slugify}"
-            // );
+            builder.MapDynamicControllerRoute<LocalizedRouteValueTransformer>(
+               "{area:exists:slugify}/{controller:slugify}/{eid:int}/{action:slugify}"
+            );
 
-            // builder.MapDynamicControllerRoute<LocalizedRouteValueTransformer>(
-            //    "{area:exists:slugify}/{controller:slugify}/{action:slugify}"
-            //);
-
-            // builder.MapDynamicControllerRoute<LocalizedRouteValueTransformer>(
-            //    "{controller:slugify}/{eid:int}/{action:slugify}"
-            // );
-
-            // builder.MapDynamicControllerRoute<LocalizedRouteValueTransformer>(
-            //    "{controller:slugify}"
-            //  );
-
-            // builder.MapDynamicControllerRoute<LocalizedRouteValueTransformer>(
-            //    "{controller:slugify}/{action:slugify}"
-            // );
-
-            // builder.MapDynamicControllerRoute<LocalizedRouteValueTransformer>(
-            //     "{controller=Home}/{action=Index}"
-            //  );
-
-            builder.MapControllerRoute(
-               name: "SubAreaSubIdRoutes",
-               pattern: "{area:exists:slugify}/{eid:int}/{subarea:exists:slugify}/{controller:slugify}/{childid:int}/{action:slugify=Item}"
+            builder.MapDynamicControllerRoute<LocalizedRouteValueTransformer>(
+               "{area:exists:slugify}/{controller:slugify}/{action:slugify}"
            );
 
-            builder.MapControllerRoute(
-                name: "SubAreaSubRoutes",
-                pattern: "{area:exists:slugify}/{subarea:exists:slugify}/{controller:slugify}/{action:slugify=Index}"
+            builder.MapDynamicControllerRoute<LocalizedRouteValueTransformer>(
+               "{controller:slugify}/{eid:int}/{action:slugify}"
             );
 
-
-            builder.MapControllerRoute(
-               name: "SubAreaControllerIdRoutes",
-               pattern: "{area:exists:slugify}/{eid:int}/{subarea:exists:slugify}/{controller:slugify}/{action:slugify=Index}"
-            );
-
-            builder.MapControllerRoute(
-               name: "AreaSubIdRoutes",
-               pattern: "{area:exists:slugify}/{eid:int}/{controller:slugify}/{childid:int}/{action:slugify=Item}"
-            );
-
-            builder.MapControllerRoute(
-               name: "AreaIdRoutes",
-               pattern: "{area:exists:slugify}/{eid:int}/{controller:slugify}/{action:slugify=Index}"
-            );
-
-            builder.MapControllerRoute(
-               name: "AreaControllerIdRoutes",
-               pattern: "{area:exists:slugify}/{controller:slugify}/{eid:int}/{action:slugify=Index}"
-            );
-
-            builder.MapControllerRoute(
-               name: "AreaHome",
-               pattern: "{area:exists:slugify}/{controller:slugify}/{action:slugify}",
-               defaults: new { action = "Index" }
-           );
-
-            builder.MapControllerRoute(
-                name: "ControllerItemRoutes",
-                pattern: "{controller:slugify}/{eid:int}/{action:slugify=Item}"
-            );
-
-            builder.MapControllerRoute(
-                 name: "DefaultAction",
-                 pattern: "{controller:slugify}",
-                 defaults: new { action = "Index" }
+            builder.MapDynamicControllerRoute<LocalizedRouteValueTransformer>(
+               "{controller:slugify}"
              );
 
-            builder.MapControllerRoute(
-                name: "DefaultHomeAction",
-                pattern: "{action:slugify}",
-                defaults: new { controller = "Home" }
+            builder.MapDynamicControllerRoute<LocalizedRouteValueTransformer>(
+               "{controller:slugify}/{action:slugify}"
             );
 
-            builder.MapControllerRoute(
-                name: "DefaultControllerAction",
-                pattern: "{controller:slugify}/{action:slugify}"
-            );
+            builder.MapDynamicControllerRoute<LocalizedRouteValueTransformer>(
+                "{controller=Home}/{action=Index}"
+             );
 
-            builder.MapControllerRoute(
-              name: "Home",
-              pattern: "",
-              defaults: new { controller = "Home", action = "Index" }
-            );
+           // builder.MapControllerRoute(
+           //    name: "SubAreaSubIdRoutes",
+           //    pattern: "{area:exists:slugify}/{eid:int}/{subarea:exists:slugify}/{controller:slugify}/{childid:int}/{action:slugify=Item}"
+           //);
+
+           // builder.MapControllerRoute(
+           //     name: "SubAreaSubRoutes",
+           //     pattern: "{area:exists:slugify}/{subarea:exists:slugify}/{controller:slugify}/{action:slugify=Index}"
+           // );
+
+
+           // builder.MapControllerRoute(
+           //    name: "SubAreaControllerIdRoutes",
+           //    pattern: "{area:exists:slugify}/{eid:int}/{subarea:exists:slugify}/{controller:slugify}/{action:slugify=Index}"
+           // );
+
+           // builder.MapControllerRoute(
+           //    name: "AreaSubIdRoutes",
+           //    pattern: "{area:exists:slugify}/{eid:int}/{controller:slugify}/{childid:int}/{action:slugify=Item}"
+           // );
+
+           // builder.MapControllerRoute(
+           //    name: "AreaIdRoutes",
+           //    pattern: "{area:exists:slugify}/{eid:int}/{controller:slugify}/{action:slugify=Index}"
+           // );
+
+           // builder.MapControllerRoute(
+           //    name: "AreaControllerIdRoutes",
+           //    pattern: "{area:exists:slugify}/{controller:slugify}/{eid:int}/{action:slugify=Index}"
+           // );
+
+           // builder.MapControllerRoute(
+           //    name: "AreaHome",
+           //    pattern: "{area:exists:slugify}/{controller:slugify}/{action:slugify}",
+           //    defaults: new { action = "Index" }
+           //);
+
+           // builder.MapControllerRoute(
+           //     name: "ControllerItemRoutes",
+           //     pattern: "{controller:slugify}/{eid:int}/{action:slugify=Item}"
+           // );
+
+           // builder.MapControllerRoute(
+           //      name: "DefaultAction",
+           //      pattern: "{controller:slugify}",
+           //      defaults: new { action = "Index" }
+           //  );
+
+           // builder.MapControllerRoute(
+           //     name: "DefaultHomeAction",
+           //     pattern: "{action:slugify}",
+           //     defaults: new { controller = "Home" }
+           // );
+
+           // builder.MapControllerRoute(
+           //     name: "DefaultControllerAction",
+           //     pattern: "{controller:slugify}/{action:slugify}"
+           // );
+
+           // builder.MapControllerRoute(
+           //   name: "Home",
+           //   pattern: "",
+           //   defaults: new { controller = "Home", action = "Index" }
+           // );
 
 
             return builder;
